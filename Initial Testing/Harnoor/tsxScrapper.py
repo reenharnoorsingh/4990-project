20ae7eca40e23f1cc80d6f5d29d03117facbd627
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2024-02-27 20:09:29 -0500

    changing it to yfinance format

--- Initial Testing/Harnoor/tsxScrapper.py
+++ Initial Testing/Harnoor/tsxScrapper.py
@@ -5,13 +5,16 @@ def get_tsx_tickers():
     url = "https://www.tsx.com/json/company-directory/search/tsx/^*"
     response = requests.get(url)
     data = response.json()
-    tickers = [item['symbol'] for item in data['results']]
+    tickers = [item['symbol'] for item in data['results'] if '.' not in item['symbol']]
     return tickers
 
 tsx_tickers = get_tsx_tickers()
 
 # Convert the list to a DataFrame
 df = pd.DataFrame(tsx_tickers, columns=['Ticker'])
+
+# Add '.TO' to the end of each ticker
+df['Ticker'] = df['Ticker'].apply(lambda x: x + '.TO')
 
 # Save the DataFrame to a CSV file
 df.to_csv('tsx_tickers.csv', index=False)
