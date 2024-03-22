a0560d2dfb46e654a22e4e86a3bbdedd390ecfa4
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2024-03-22 08:05:52 -0400

    FINALLYYYYYYY

--- backend2.py
+++ backend2.py
@@ -2,9 +2,8 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 import requests
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from datetime import timedelta
+from datetime import datetime
 
 def get_tsx_tickers():
     url = "https://www.tsx.com/json/company-directory/search/tsx/^*"
@@ -16,6 +15,11 @@ def get_tsx_tickers():
 def fetch_data(ticker_symbol):
     ticker_data = yf.Ticker(ticker_symbol)
     return ticker_data.history(period='1d', start='2020-1-1', end='2023-12-31')
+
+def fetch_latest_data(ticker_symbol):
+    today = datetime.today().strftime('%Y-%m-%d')
+    ticker_data = yf.Ticker(ticker_symbol)
+    return ticker_data.history(period='1d', start='2024-01-01', end=today)
 
 def calculate_sma(data, window):
     return data['Close'].rolling(window=window).mean()
@@ -38,37 +42,8 @@ def calculate_macd(data, short_window, long_window, signal_window):
     return macd, signal_line
 
 def calculate_bollinger_bands(data, window):
-    rolling_mean = data['Close'].rolling(window=window).mean()
+    middle_band = data['Close'].rolling(window=window).mean()
     rolling_std = data['Close'].rolling(window=window).std()
-    upper_band = rolling_mean + (rolling_std * 2)
-    lower_band = rolling_mean - (rolling_std * 2)
+    upper_band = middle_band + (rolling_std * 2)
+    lower_band = middle_band - (rolling_std * 2)
     return upper_band, lower_band
-
-def create_lstm_dataset(data, time_step=100):
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
-
-    X_data, y_data = [], []
-    for i in range(len(scaled_data) - time_step - 1):
-        X_data.append(scaled_data[i:(i + time_step), 0])
-        y_data.append(scaled_data[i + time_step, 0])
-    
-    X_data, y_data = np.array(X_data), np.array(y_data)
-    X_data = X_data.reshape(X_data.shape[0], X_data.shape[1], 1)
-    return X_data, y_data, scaler
-
-def create_lstm_model(input_shape):
-    model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=input_shape))
-    model.add(LSTM(units=50))
-    model.add(Dense(units=1))
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    return model
-
-def train_lstm_model(model, X_train, y_train, epochs=1, batch_size=1):
-    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=0)
-    return model
-
-def predict_lstm(model, X_data, scaler):
-    predictions = model.predict(X_data)
-    return scaler.inverse_transform(predictions)
